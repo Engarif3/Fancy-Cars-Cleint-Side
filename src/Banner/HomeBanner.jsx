61a44362e949329091cda0dc470e4d8465311ed5
Md.Arifur Rahman <arif.aust.eng@gmail.com>
2023-05-20 22:50:53 +0200

    banner tag lines added

--- src/Banner/HomeBanner.jsx
+++ src/Banner/HomeBanner.jsx
@@ -6,58 +6,62 @@ import { Carousel } from "react-bootstrap";
 
 const HomeBanner = () => {
   return (
-    <div className="mt-4" >
+    <div className="mt-4">
       <Carousel>
-      <Carousel.Item interval={2000}>
-      <div className="d-flex" style={{height:"500px"}}>
-       <img
-          className="d-block img-fluid w-50 h-100 "
-          src={img1}
-          alt="Second slide"
-          
-        />
-        <div className="text-black w-50 p-5 d-flex justify-content-center align-items-center "> <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, voluptatibus qui. Illo dicta obcaecati porro aperiam molestiae dignissimos officia ipsam.</p> </div>
-       </div>
-        {/* <Carousel.Caption className="text-black">
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption> */}
-      </Carousel.Item>
-      <Carousel.Item interval={2000}>
-      <div className="d-flex" style={{height:"500px"}}>
-       <img
-          className="d-block img-fluid w-50 h-100"
-          src={img2}
-          alt="Second slide"
-          
-        />
-           <div className="text-black w-50 p-5 d-flex justify-content-center align-items-center "> <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, voluptatibus qui. Illo dicta obcaecati porro aperiam molestiae dignissimos officia ipsam.</p> </div>
-       </div>
-        
-        {/* <Carousel.Caption className="text-black">
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption> */}
-      </Carousel.Item>
-      <Carousel.Item interval={2000}>
-      <div className="d-flex" style={{height:"500px"}}>
-       <img
-          className="d-block img-fluid w-50 h-100"
-          src={img3}
-          alt="Second slide"
-          
-        />
-           <div className="text-black w-50 p-5 d-flex justify-content-center align-items-center "> <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, voluptatibus qui. Illo dicta obcaecati porro aperiam molestiae dignissimos officia ipsam.</p> </div>
-       </div>
-        {/* <Carousel.Caption className="text-black">
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption> */}
-      </Carousel.Item>
-    </Carousel>
-      
+        <Carousel.Item interval={2000}>
+          <div className="d-flex" style={{ height: "500px" }}>
+            <img
+              className="d-block img-fluid w-50 h-100 "
+              src={img1}
+              alt="Second slide"
+            />
+            <div className="text-black w-50 p-5 d-flex justify-content-center align-items-center ">
+              <div className="fst-italic">
+                <h1 className="text-primary">Accelerate the Fun with......</h1>
+                <h2 className="ms-5 text-warning">
+                Fancy Cars that Bring Smiles to Kids!
+                </h2>
+              </div>
+            </div>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item interval={2000}>
+          <div className="d-flex" style={{ height: "500px" }}>
+            <img
+              className="d-block img-fluid w-50 h-100"
+              src={img2}
+              alt="Second slide"
+            />
+             <div className="text-black w-50 p-5 d-flex justify-content-center align-items-center ">
+              <div className="fst-italic">
+                <h1 className="text-primary">Fueling Excitement with ......</h1>
+                <h2 className="ms-5 text-warning">
+                Toy Cars for Kids to have Big Adventures!
+                </h2>
+              </div>
+            </div>
+          </div>
+
+        </Carousel.Item>
+        <Carousel.Item interval={2000}>
+          <div className="d-flex" style={{ height: "500px" }}>
+            <img
+              className="d-block img-fluid w-50 h-100"
+              src={img3}
+              alt="Second slide"
+            />
+            <div className="text-black w-50 p-5 d-flex justify-content-center align-items-center ">
+              <div className="fst-italic">
+                <h1 className="text-primary">Unleash the Joyride......</h1>
+                <h2 className="ms-5 text-warning">
+                  Discover Endless Fun with Fancy Cars!
+                </h2>
+              </div>
+            </div>
+          </div>
+         
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 };
